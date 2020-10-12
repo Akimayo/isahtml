38b4e9331f77ccea22025eea93ff618baa191e77
Michal Ciesla <ciesla.m@outlook.com>
2020-10-12 18:54:18 +0200

    Showcase, code cleanup

---
+++ src/libs/p5/Animation.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import p5js from 'p5';
+
+function Animation(props) {
+    const p5ref = React.createRef();
+    let p5inst;
+    useEffect(() => {
+        p5inst = new p5js(props.sketch, p5ref.current);
+        return () => (p5inst = undefined);
+    }, []);
+    return (<div ref={p5ref} />);
+}
+export default Animation;
---
+++ src/showcaseSketch.js
@@ -0,0 +1,21 @@
+const sketch = (p) => {
+    const textSize = 48;
+    const wobble = 4;
+    let framecount = 0;
+    const text = 'Hello!';
+    p.setup = () => {
+        p.createCanvas(400, 200);
+        p.frameRate(2);
+        p.textSize(textSize);
+        p.textFont('monospace');
+        p.textStyle(p.BOLD);
+    }
+    p.draw = () => {
+        p.background(31, 121, 150);
+        console.log((p.height - textSize))
+        for(let i = 1; i <= text.length; i++) 
+            p.text(text.substring(i-1,i), i*textSize, (p.height/2)+wobble*(i%2^framecount));
+        framecount = (framecount + 1) % 2;
+    }
+}
+export default sketch;
