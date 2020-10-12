860ff6d303137b5e43510b5edf4b795ed8bf4e1f
Michal Ciesla <ciesla.m@outlook.com>
2020-10-12 18:54:06 +0200

    Add i18next

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './libs/bootstrap/bootstrap';
+import './libs/i18next';
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div className="spinner-border ml-2" />}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
